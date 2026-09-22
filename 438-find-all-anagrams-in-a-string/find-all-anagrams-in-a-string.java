9b5c0e059872925e2c17134f299eea52989437f5
Sarvesjr <sarvessrirangan8@gmail.com>
2026-09-22 23:43:54 +0530

    Time: 68 ms (17.19%) | Memory: 48.4 MB (5.47%) - LeetSync

---
+++ 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.java
@@ -0,0 +1,36 @@
+class Solution {
+    public List<Integer> findAnagrams(String s, String p) {
+        HashMap<Character,Integer> mapP = new HashMap<>();
+        HashMap<Character,Integer> mapW = new HashMap<>();
+
+        //add p into mapP
+        for (int i=0;i<p.length();i++){
+            mapP.put(p.charAt(i),mapP.getOrDefault(p.charAt(i),0)+1);
+        }
+        //declare variables
+        int left=0;
+        int right=0;
+        List<Integer> result = new ArrayList<>(); //syntax doubt
+
+        while(right<s.length()){
+            mapW.put(s.charAt(right),mapW.getOrDefault(s.charAt(right),0)+1);
+            //window length check
+            if((right-left+1)>p.length()){
+                char ch = s.charAt(left);
+                mapW.put(ch,mapW.get(ch)-1);
+                if(mapW.get(ch)==0){
+                    mapW.remove(ch);
+                }
+                left++;
+            }
+            //check frequencies
+            if((right-left+1)==p.length()){
+                if (mapP.equals(mapW)){
+                    result.add(left);
+                }
+            }
+            right++;
+        }
+        return result;
+    }
+}
