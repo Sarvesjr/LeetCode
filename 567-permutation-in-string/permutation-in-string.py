ccd4435f4ee17bff50686a59e0ca7d7011e834cb
Sarvesjr <sarvessrirangan8@gmail.com>
2026-09-22 10:19:50 +0530

    Time: 31 ms (41.07%) | Memory: 12.5 MB (89.98%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.py
+++ 567-permutation-in-string/permutation-in-string.py
@@ -11,22 +11,26 @@ class Solution(object):
         map1={}
         map2={}
 
+        #add map1
         for ch in s1:
             map1[ch]=map1.get(ch,0)+1
 
         left=0
         right=0
         k=len(s1)
-
+        
         while right < len(s2):
+            #add right character in map2:
             map2[s2[right]]=map2.get(s2[right],0)+1
 
+            #if window size exceeds k:
             if(right-left+1)>k:
-                map2[s2[left]]-=1
+                map2[s2[left]]-=1 #removing left from freq map
                 if map2[s2[left]]==0:
-                    del map2[s2[left]]
+                    del map2[s2[left]] #removing character from map if freq 0
                 left+=1
             
+            #if window size equals k: check if both maps match:
             if(right-left+1)==k:
                 if map1==map2:
                     return True
