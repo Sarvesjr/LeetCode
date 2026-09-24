473363dd778ecab77d0c49f087200cfc7c252ac6
Sarvesjr <sarvessrirangan8@gmail.com>
2026-09-24 15:50:16 +0530

    Time: 36 ms (60.91%) | Memory: 14.6 MB (73.46%) - LeetSync

--- 560-subarray-sum-equals-k/subarray-sum-equals-k.py
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.py
@@ -6,10 +6,12 @@ class Solution(object):
         :rtype: int
         """
         count={0: 1}
-        total,answer=0,0
-        for num in nums:
-            total+= num
-            if total-k in count:
-                answer += count[total-k]
-            count[total] = count.get(total,0)+1
+        curr_prefix,answer=0,0
+        for num in nums: #take each number
+            curr_prefix += num #update current prefix sum
+
+            if curr_prefix-k in count: #check if we have already seen the reuired previous sum
+                answer += count[curr_prefix-k] #add how many times we saw it
+
+            count[curr_prefix] = count.get(curr_prefix,0)+1 #update the sum
         return answer
