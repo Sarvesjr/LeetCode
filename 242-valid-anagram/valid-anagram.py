b34106bd6379235265bfba4873c264590dad53cb
Sarvesjr <sarvessrirangan8@gmail.com>
2026-09-24 14:22:56 +0530

    Time: 28 ms (9.35%) | Memory: 13.4 MB (30.25%) - LeetSync

---
+++ 242-valid-anagram/valid-anagram.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def isAnagram(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+        if len(s)!=len(t):
+            return False
+        a=list(s)
+        b=list(t)
+        a.sort()
+        b.sort()
+        if a==b:
+            return True
+        return False
