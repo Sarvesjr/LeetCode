5c303965383180d0aa86f679045ed167017137be
Sarvesjr <sarvessrirangan8@gmail.com>
2026-09-24 14:25:14 +0530

    Time: 1 ms (62.97%) | Memory: 13.2 MB (38.24%) - LeetSync

---
+++ 1-two-sum/two-sum.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        map={}
+        for i in range(len(nums)):
+            complement=target-nums[i]
+            if complement in map:
+                return [map[complement],i]
+            map[nums[i]]=i
