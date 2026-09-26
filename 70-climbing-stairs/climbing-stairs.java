b4f9b640e2cde9203e5b27e74087d3d52feaf7bd
sarves srirangan <sarvessrirangan8@gmail.com>
2026-09-26 15:32:47 +0530

    Time: 0 ms (100.00%) | Memory: 42.1 MB (36.12%) - LeetSync

---
+++ 70-climbing-stairs/climbing-stairs.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int climbStairs(int n) {
+        int curr=1;
+        int prev=1;
+        for (int i=0;i<n-1;i++){
+            int temp = curr;
+            curr = curr + prev;
+            prev = temp;
+        }
+        return curr;
+    }
+}
